030c0e7db63dc1bb0fbf57623eab131595eb92d5
cchiasson <chiasson@ualberta.ca>
2018-02-15 14:15:45 -0700

    added files

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Lab3IO.h"
+#include "timer.h"
+
+int main(int argc, char* argv[]) {
+  int num_threads = argv[1];
+  return 0;
+}
